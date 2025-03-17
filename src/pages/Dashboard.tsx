b2cb89d8b35e8c70c579412106bb3fdc2c48004a
Jacob Vendramin <jacob@vendramin.work>
2025-03-17 10:34:30 -0700

    test

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import { Analytics } from '@vercel/analytics/react'
 import { useNavigate } from "react-router-dom";
 import { useProjectStats } from "@/hooks/useProjectStats";
 import StatsCard from "@/components/dashboard/StatsCard";
@@ -26,6 +27,7 @@ const Dashboard = () => {
 
   return (
     <div className="p-8 h-full overflow-auto">
+      <Analytics />
       <h1 className="text-2xl font-bold mb-6">Dashboard</h1>
       
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6 mb-6">
